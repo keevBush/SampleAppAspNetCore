d1db11047fd451f3703cd70e4192590c4186271e
Kevin Bush <keevbush@hotmail.com>
2022-06-29 19:30:54 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SampleAppAspNetCore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
